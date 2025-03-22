d09834692e54dff2d50d2090ef7efcc8ad99bdb5
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2025-03-23 05:42:06 +0900

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 #include <USB.h>
 #include <USBHIDGamepad.h>
+#include "Gamepad.h"
 
-USBHIDGamepad gamepad;
+Gamepad gamepad;
 
-void testAxises();
+void testAxises8();
+void testAxises16();
 void testKeys();
 void testHats();
 
@@ -18,7 +20,7 @@ void setup() {
 
 void loop() {
     log_d("test: leftStick");
-    testAxises();
+    testAxises16();
     delay(1000);
     log_d("test: hat");
     testHats();
@@ -29,7 +31,7 @@ void loop() {
     testKeys();
 }
 
-void testAxises(){
+void testAxises8(){
   // -255から255までの値を1つずつ入力
   for(int i = INT8_MIN; i <= INT8_MAX; i++){
     gamepad.leftStick(i, i);
@@ -54,6 +56,14 @@ void testAxises(){
   gamepad.rightStick(0, 0);
   gamepad.leftTrigger(0);
   gamepad.rightTrigger(0);
+}
+void testAxises16(){
+  // -32767から32768までの値を1つずつ入力
+  for(int i = INT16_MIN; i <= INT16_MAX; i++){
+    gamepad.setAxes(i, i, i, i, i, i, i, i);
+  }
+  delay(100);
+  gamepad.setAxes(0, 0, 0, 0, 0, 0, 0, 0);
 }
 void testKeys(){
   // 0から32までのキーを1つずつ押す
