3cebda95f69ef034bb182fd64b565e52d5deba3a
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2025-03-23 05:07:19 +0900

    update gamepad test

--- src/main.cpp
+++ src/main.cpp
@@ -33,23 +33,27 @@ void testAxises(){
   // -255から255までの値を1つずつ入力
   for(int i = INT8_MIN; i <= INT8_MAX; i++){
     gamepad.leftStick(i, i);
-    delay(1);
+    delay(10);
   }
   delay(100);
   for(int i = INT8_MIN; i <= INT8_MAX; i++){
     gamepad.rightStick(i, i);
-    delay(1);
+    delay(10);
   }
   delay(100);
   for(int i = INT8_MIN; i <= INT8_MAX; i++){
     gamepad.leftTrigger(i);
-    delay(1);
+    delay(10);
   }
   delay(100);
   for(int i = INT8_MIN; i <= INT8_MAX; i++){
     gamepad.rightTrigger(i);
-    delay(1);
+    delay(10);
   }
+  gamepad.leftStick(0, 0);
+  gamepad.rightStick(0, 0);
+  gamepad.leftTrigger(0);
+  gamepad.rightTrigger(0);
 }
 void testKeys(){
   // 0から32までのキーを1つずつ押す
