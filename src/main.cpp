2d214688a722eb22849df6d8bf10b6734f56818d
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2025-03-23 02:16:33 +0900

    add test gamepad

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,69 @@
 #include <Arduino.h>
+#include <USB.h>
+#include <USBHIDGamepad.h>
 
-// put function declarations here:
-int myFunction(int, int);
+USBHIDGamepad gamepad;
+
+void testAxises();
+void testKeys();
+void testHats();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    log_d("setup");
+    gamepad.begin();
+    USB.begin();
+    delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    log_d("test: leftStick");
+    testAxises();
+    delay(1000);
+    log_d("test: hat");
+    testHats();
+    delay(1000);
+    gamepad.hat(0);
+    log_d("test: btn");
+    delay(1000);
+    testKeys();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void testAxises(){
+  // -255から255までの値を1つずつ入力
+  for(int i = INT8_MIN; i <= INT8_MAX; i++){
+    gamepad.leftStick(i, i);
+    delay(1);
+  }
+  delay(100);
+  for(int i = INT8_MIN; i <= INT8_MAX; i++){
+    gamepad.rightStick(i, i);
+    delay(1);
+  }
+  delay(100);
+  for(int i = INT8_MIN; i <= INT8_MAX; i++){
+    gamepad.leftTrigger(i);
+    delay(1);
+  }
+  delay(100);
+  for(int i = INT8_MIN; i <= INT8_MAX; i++){
+    gamepad.rightTrigger(i);
+    delay(1);
+  }
+}
+void testKeys(){
+  // 0から32までのキーを1つずつ押す
+  for(int i = 0; i < 32; i++){
+    gamepad.pressButton(i);
+    delay(100);
+    gamepad.releaseButton(i);
+  }
+}
+void testHats(){
+  // 0から7までのキーを1つずつ押す
+  for(int i = 0; i < 8; i++){
+    gamepad.hat(i);
+    delay(100);
+  }
+  gamepad.hat(0);
 }
