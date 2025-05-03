fe57cd592b35c1c05d31bd43f5740603c1667840
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2025-05-04 02:19:02 +0900

    add mascon class

---
+++ src/Mascon.cpp
@@ -0,0 +1,159 @@
+#include "Mascon.h"
+#include "Settings.h"
+#include <Arduino.h>
+#include <array>
+#include <driver/gpio.h>
+
+using std::array;
+using namespace Settings::Pins;
+
+#pragma region マスコン操作クラス
+Mascon::Mascon() {
+    pinStatusStore = std::array<std::array<bool, PINNAME_MAX>, ARRAYNAME_MAX>{};
+    for (auto &&i : pinStatusStore) {
+        i.fill(true);
+    }
+}
+
+Mascon::~Mascon() {}
+
+bool Mascon::samplingPins() {
+    // スキャン後変化有無
+    bool isChanged = false;
+
+    // スキャン結果準備：シフト
+    pinStatusStore.at(PREVIOUS) = pinStatusStore.at(CURRENT);
+    // for (int i = pinStatusStore.size() - 1; i >= 1; i--) {
+    //     pinStatusStore.at(i) = pinStatusStore.at(i - 1);
+    // }
+
+    // スキャン
+    for (size_t i = 0; i < PINNAME_MAX; i++) {
+        pinStatusStore.at(CURRENT).at(i) = digitalRead(pinNums.at(i));
+    }
+
+    // スキャン結果比較
+    for (size_t i = 0; i < pinStatusStore.at(CURRENT).size(); i++) {
+        // 値の変化は2回一致で採用
+        bool currentStatsus = pinStatusStore.at(CURRENT).at(i);
+        if (currentStatsus != pinStatusStore.at(OUTPUT_VALUE).at(i) &&
+            currentStatsus == pinStatusStore.at(PREVIOUS).at(i)) {
+            pinStatusStore.at(OUTPUT_VALUE).at(i) = currentStatsus;
+            isChanged = true;
+        }
+    }
+
+    return isChanged;
+}
+
+int32_t Mascon::getReverserInt32() const {
+    // 入力をビット列で取得
+    uint8_t status = pinStatusStore.at(OUTPUT_VALUE).at(R_F) << 0 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(R_R) << 1;
+
+    // レバーサー
+    int32_t reverser = 0;
+    switch (status) {
+    case 0b10: // 前進
+        reverser = INT32_MAX;
+        break;
+    case 0b01: // 後退
+        reverser = INT32_MIN;
+        break;
+    default: // 中立
+        reverser = 0;
+        break;
+    }
+    
+    return reverser;
+}
+
+int32_t Mascon::getPowerNotchInt32() const {
+    // 入力をビット列で取得
+    uint8_t status = pinStatusStore.at(OUTPUT_VALUE).at(P_1) << 0 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(P_2) << 1 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(P_3) << 2 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(P_4) << 3 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(P_5) << 4;
+
+    // ノッチ数
+    uint8_t powerNotch = 0;
+    switch (status) {
+    case 0b00000: // P5
+        powerNotch = 5;
+        break;
+    case 0b10000: // P4
+        powerNotch = 4;
+        break;
+    case 0b11000: // P3
+        powerNotch = 3;
+        break;
+    case 0b11100: // P2
+        powerNotch = 2;
+        break;
+    case 0b11110: // P1
+        powerNotch = 1;
+        break;
+    default: // P0
+        powerNotch = 0;
+        break;
+    }
+
+    // ノッチ数を-32767から32768までの値に変換
+    return static_cast<int32_t>(INT32_MAX * powerNotch / POWERNOTCH_MAX);
+}
+
+int32_t Mascon::getBrakeNotchInt32() const {
+    // 入力をビット列で取得
+    // 赤x：1
+    // 青o：0
+    uint8_t status = pinStatusStore.at(OUTPUT_VALUE).at(B_EB) << 0 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(B_NB) << 1 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(B_YB) << 2 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(B_1_5_6_7) << 3 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(B_1_2_3_6_7) << 4 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(B_3_4_5_6_7) << 5 |
+                     pinStatusStore.at(OUTPUT_VALUE).at(B_7) << 6;
+
+    // ノッチ数
+    uint8_t brakeNotch = 0;
+    switch (status) {
+    case 0b1111111: // B0
+        brakeNotch = 0;
+        break;
+    case 0b1111011: // YB
+        brakeNotch = 1;
+        break;
+    case 0b1100101: // B1
+        brakeNotch = 2;
+        break;
+    case 0b1101101: // B2
+        brakeNotch = 3;
+        break;
+    case 0b1001101: // B3
+        brakeNotch = 4;
+        break;
+    case 0b1011101: // B4
+        brakeNotch = 5;
+        break;
+    case 0b1010101: // B5
+        brakeNotch = 6;
+        break;
+    case 0b1000101: // B6
+        brakeNotch = 7;
+        break;
+    case 0b0000101: // B7
+        brakeNotch = 8;
+        break;
+    case 0b1111110: // EB
+        brakeNotch = 9;
+        break;
+    default: // EB
+        brakeNotch = 9;
+        break;
+    }
+
+    // ノッチ数を-32767から32768までの値に変換
+    return static_cast<int32_t>(INT32_MAX * brakeNotch / BRAKENOTCH_MAX);
+}
+#pragma endregion
---
+++ src/Mascon.h
@@ -0,0 +1,60 @@
+#pragma onece
+
+#include "settings.h"
+#include <array>
+
+class Mascon {
+  private:
+    // GPIOピンの状態を格納する配列
+    std::array<std::array<bool, Settings::Pins::PINNAME_MAX>, Settings::Pins::ARRAYNAME_MAX> pinStatusStore;
+
+    // スキャン周期の分解能
+    constexpr static size_t DEVIDER = 16;
+    // ノッチ最大値
+    constexpr static uint8_t POWERNOTCH_MAX = 5;
+    // ブレーキ最大値（抑速・非常含む）
+    constexpr static uint8_t BRAKENOTCH_MAX = 7;
+
+  public:
+    /**
+     * @brief コンストラクタ
+     */
+    Mascon();
+
+    /**
+     * @brief デストラクタ
+     */
+    ~Mascon();
+
+    /**
+     * @brief ピンの初期化
+     */
+    void setupPins() const;
+
+    /**
+     * @brief ピンのスキャン
+     * @return スキャン後の状態変化有無
+     */
+    bool samplingPins();
+
+    /**
+     * @brief レバーサーの状態を取得
+     * @return レバーサーの状態
+     * @note 前進：INT32_MAX、後進：INT32_MIN、中立：0
+     */
+    int32_t getReverserInt32() const;
+
+    /**
+     * @brief パワーノッチの状態を取得
+     * @return パワーノッチの状態
+     * @note INT32_MINからINT32_MAXまでの値に変換
+     */
+    int32_t getPowerNotchInt32() const;
+
+    /**
+     * @brief ブレーキノッチの状態を取得
+     * @return ブレーキノッチの状態
+     * @note -INT32_MINからINT32_MAXまでの値に変換
+     */
+    int32_t getBrakeNotchInt32() const;
+};
