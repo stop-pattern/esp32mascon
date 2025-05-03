8b773b07fd5169a337b7b7392e68e87c7e011826
stop-pattern <40131396+stop-pattern@users.noreply.github.com>
2025-05-04 03:54:56 +0900

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
-#include "PinScan.h"
+// #include "PinScan.h"
 #include "UsbTask.h"
 #include <Arduino.h>
 
 namespace {
-TaskHandle_t taskHandlePinScan = nullptr;
+// TaskHandle_t taskHandlePinScan = nullptr;
 TaskHandle_t taskHandleUsb = nullptr;
+TaskHandle_t taskHandleSampling = nullptr;
+
+/// @brief 割り込み用タイマー
+hw_timer_t *timer = NULL;
+
+/// @brief タイマー割り込みのディバイダー
+/// @note 1us単位
+const uint16_t timerCount = getApbFrequency() / 1000000;
 } // namespace
 
 void setup() {
     Serial.begin(115200);
     log_d("setup");
 
-    xTaskCreate(PinScanTask, "PinScanTask", 2048, NULL, 1, &taskHandlePinScan);
+    // xTaskCreate(PinScanTask, "PinScanTask", 2048, NULL, 1, &taskHandlePinScan);
     xTaskCreate(UsbTask, "UsbTask", 2048, NULL, 1, &taskHandleUsb);
+    xTaskCreate(SamplingTask, "SamplingTask", 2048, NULL, 1,
+                &taskHandleSampling);
+
+    // タイマー割り込みの設定
+    timer = timerBegin(0, timerCount, true);
+    timerAttachInterrupt(timer, &onTimer, true);
+    timerAlarmWrite(timer, 1000, true);
+    timerAlarmEnable(timer);
 }
 
 void loop() {
